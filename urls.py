fca8ec96a043f43a2c67f4cbb4eb0aefb2fe6843
Russell Little <Russell Little>
2016-10-27 22:49:35 -0500

    moved login urls to this project. still need to include LOGIN_REDIRECT_URL in root settings

--- urls.py
+++ urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'respondents/(?P<respondentid>[0-9]+)', views.respondent,
+    url(r'^login/', auth_views.login, name='login'),
+    url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^respondents/(?P<respondentid>[0-9]+)', views.respondent,
         name='respondent'),
-    url(r'respondents$', views.respondents, name='respondents'),
+    url(r'^respondents$', views.respondents, name='respondents'),
     url(r'^surveys$', views.surveys, name='surveys'),
     url(r'^surveys/(?P<surveyid>[0-9]+)$', views.survey, name='survey'),
     url(r'^surveys/(?P<surveyid>[0-9]+)/submit$', views.submit_survey,
